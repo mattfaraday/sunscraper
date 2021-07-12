5b80af02bb2cb6c0d6177a7c63b0c917be72edd0
mattfaraday <matt@faraday.at>
2021-07-12 17:15:47 +0100

    Make moar better

--- ethersun.py
+++ ethersun.py
@@ -6,41 +6,71 @@
 # it also requires the t-rex miner to be up and running (but can be paused)
 # I suggest you cron this script
 
+import urllib3
 import requests
 from datetime import datetime
 from requests.auth import HTTPDigestAuth
 from bs4 import BeautifulSoup
 
-####### CHANGE THESE #######
+### Change these ###
+
 username = "my@email.com"
-password = "password"
-trexIP = "192.168.0.3"
-trexPORT = "4067"
-
-## This is the amount of power your miner consumes when mining ..."
-miningpower = 150
-############################
-
-
+password = "mypassword"
+trexIP = "192.168.0.2"
+trexPort = "4067"
 url = 'http://live.myimmersun.com'
+
+
+## The amount of power your graphics card/computer uses to mine with, in watts
+miningpower = 350
+###
+
+
+##Scrape the immersun website
 page = requests.get(url, auth=HTTPDigestAuth(username, password))
 soup = BeautifulSoup(page.text, 'html.parser')
-
 import_power = soup.find(class_='container')
 import_power_item = import_power.find_all('p')
 import_value = import_power_item[0].contents[0]
 production_value = import_power_item[1].contents[0]
 consumption_value = import_power_item[2].contents[0]
-
 if "Hot" not in import_power_item[3].contents[0]:
      divert_value = import_power_item[3].contents[0]
 else:
      divert_value = 0
+
 pproduction = int(str(production_value).rstrip('W'))
 pconsumption = int(str(consumption_value).rstrip('W'))
 pfree = pproduction - pconsumption
 
-if (pfree > miningpower):
-   response = requests.get("http://"+trexIP+":"+trexPORT+"/control?pause=false")
-else:
-   response = requests.get("http://"+trexIP+":"+trexPORT+"/control?pause=true")
+
+# Check current status of t-rex miner
+
+http = urllib3.PoolManager()
+minerStatusUrl = "http://"+trexIP+":"+trexPort+"/summary"
+response = http.request('GET', minerStatusUrl)
+minerStatus = response.data.decode('utf-8')
+
+if "\"paused\":true" in minerStatus:
+  mining = False
+if "\"paused\":false" in minerStatus:
+  mining = True
+
+
+###DEBUGGING
+#print("Mining = ", mining)
+#print("Production: ", pproduction)
+#print("Consumption: ", pconsumption)
+#print("Surplus: ",pfree)
+############
+
+
+if (pfree > miningpower and mining == False):
+#   print("Enough energy free to mine, starting miner")
+   response = requests.get("http://"+trexIP+":"+trexPort+"/control?pause=false")
+#   print(response)
+
+if (mining == True and pconsumption > pproduction):
+#   print("Insufficient free power to mine, pausing miner")
+   response = requests.get("http://"+trexIP+":"+trexPort+"/control?pause=true")
+#   print(response)
